646ecb939cb202d6b207b3571e230ca57f662141
samrat patel <samratpatel12x@gmail.com>
2023-09-14 18:49:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './input.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-red-900'><p>koooooo</p></div>
+     <h1 className='bg-black '>hello
+
+     </h1>
+    </>
+  )
+}
+
+export default App
