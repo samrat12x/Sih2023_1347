145a2080d09a885bbe5666dbc641d6a0bc9c8062
samrat patel <samratpatel12x@gmail.com>
2023-09-17 19:11:54 +0530

    commit 6:symptoms page functionality improvements

--- src/components/SymptomsPage/Searchbar.jsx
+++ src/components/SymptomsPage/Searchbar.jsx
@@ -1,54 +1,126 @@
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import "./Searchbar.css";
-import data from './Mockdata.json';
+import data from './MockdataV2.json';
 
 
 export default function Searchbar() {
   const [value, setValue] = useState("");
 
-  const onChange = (event) => {
+//i think in future i need to remove anyone of symptomList or symptomListObj
+  const[symptomList,setSymptomList]=useState([]);
+const [symptomListObj, setSymptomListObj] = useState({});  
+
+const onChange = (event) => {
     setValue(event.target.value);
   };
 
+
+  const onSelectingListItem= (listItem) =>{
+    setValue(listItem);
+  }
+
+
+
+
   const onSearch = (searchTerm) => {
-    setValue(searchTerm);
+   // setValue(searchTerm);
     // our api to fetch the search result
-    console.log("search ", searchTerm);
+    const isDuplicate = symptomList.some((item) => item.symptom === searchTerm);
+    
+    if (!isDuplicate && searchTerm) {
+      // Create a new dictionary item with the selected symptom and add it to the list
+      const newSymptom = { symptom: searchTerm };
+      setSymptomList([...symptomList, newSymptom]);
+      
+    }
+//    console.log(symptomList);
+
+    const isDuplicate2 = Object.keys(symptomListObj).includes(searchTerm);
+    
+    if (!isDuplicate2 && searchTerm) {
+      // Create a new dictionary item with the selected symptom and add it to the list
+      setSymptomListObj({
+        ...symptomListObj,
+        [searchTerm]: { symptom: searchTerm }
+  
+    });
+  //  console.log(symptomListObj);  
+
+}
+  
+   
+   
   };
 
-  return (
+
+  const removeSymptom = (searchTerm) => {
+    const updatedSymptomList = { ...symptomListObj };
+    delete updatedSymptomList[searchTerm];
+    setSymptomListObj(updatedSymptomList);
+  };
+
+
+
+
+
+
+  useEffect(() => {
+    console.log(symptomList);
+  }, [symptomList]);
+
+  useEffect(() => {
+    console.log(symptomListObj);
+  }, [symptomListObj]);
+
+
+
+  return (<>
     <div className="Searchbar">
       <h1>Search</h1>
 
       <div className="search-container">
         <div className="search-inner">
           <input type="text" value={value} onChange={onChange} />
-          <button onClick={() => onSearch(value)}> Search </button>
+          <button onClick={() => onSearch(value)}> ADD </button>
         </div>
         <div className="dropdown">
           {data
             .filter((item) => {
               const searchTerm = value.toLowerCase();
-              const fullName = item.full_name.toLowerCase();
+              const fullName = item.symptoms.toLowerCase();
 
               return (
+                //swapped .startsWith() with .includes() method
                 searchTerm &&
-                fullName.startsWith(searchTerm) &&
+                fullName.includes(searchTerm) &&
                 fullName !== searchTerm
               );
             })
             .slice(0, 10)
             .map((item) => (
               <div
-                onClick={() => onSearch(item.full_name)}
+                onClick={() => onSelectingListItem(item.symptoms)}
                 className="dropdown-row"
-                key={item.full_name}
+                key={item.symptoms}
               >
-                {item.full_name}
+                {item.symptoms}
               </div>
             ))}
         </div>
       </div>
     </div>
+    
+    <div>
+        <h2>Symptom List</h2>
+        <ul>
+          {Object.entries(symptomListObj).map(([searchTerm, item], index) => (
+            <li key={index}>
+              {item.symptom}
+              <button onClick={() => removeSymptom(searchTerm)}>Remove Symptom</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
